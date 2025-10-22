5f28382f2606a7977c9679a8c2a462c8b5fa4153
Michito Maeda <michito.maeda@gmail.com>
2025-10-22 12:52:15 +0900

    feat: remove generated SDK for server

--- freeeapi/generate.go
+++ freeeapi/generate.go
@@ -1,3 +1,3 @@
 package freeapi
 
-//go:generate go tool oapi-codegen -config cfg.yaml -o gen/api_gen.go api-schema.json
+//go:generate go tool oapi-codegen -generate types,client -config cfg.yaml -o gen/api_gen.go api-schema.json
